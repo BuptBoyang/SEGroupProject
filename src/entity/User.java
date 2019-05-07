0940961489c0c2d9df2022b3293b84b6a471341d
lllxxxjjj111 <50314225+lllxxxjjj111@users.noreply.github.com>
2019-05-07 18:55:36 +0800

    Update User.java

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,88 +1,131 @@
 package entity;
 
-import java.util.regex.*;
+import java.util.Calendar;
+
 
 public class User {
 	private String studentID;
 	private String email;
 	private String name;
-	private float fine;
-	private int status;
+	private int status;//2 means no using. 1 means using.
+	private Calendar cld;
+	private int[] dayUsingTime;
+	private boolean fine;
 
-	public User(String id, String name , String email) {
+	public User(String id, String name, String email) {
 		this.email = email;
 		this.name = name;
 		this.studentID = id;
+		this.dayUsingTime= new int[2];
+		this.dayUsingTime[0]=0;
+		this.dayUsingTime[1]=0;
+		this.fine=false;
+		this.status=2;
 	}
 
-	public boolean isIDLegal() {
-		if (this.studentID.length() == 9) {
-			try {
-				Integer.valueOf(this.studentID);
-				return true;
-			} catch (Exception ex) {
-			}
-		}
-		return false;
-		// the QM number should be 9 digits;
-		/*
-		 * String REGEX="^\d{9}$"; Pattern p = Pattern.compile(REGEX); Matcher
-		 * matcher=p.matcher(this.studentID); return matcher.matches();
-		 */
+	/**
+	 * @return the dayUsingTime
+	 */
+	public int[] getDayUsingTime() {
+		return dayUsingTime;
 	}
 
-	public boolean isEmailLegal() {
-		// email address must in the form of xxx@xxx.xxx,
-		//"[a-z0-9A-Z]+(.[a-z0-9A-z]{1,})?@[a-z0-9A-Z]+(.[a-z0-9A-z]{1,})?"
-		String REGEX="^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
-		Pattern p = Pattern.compile(REGEX);
-		Matcher matcher = p.matcher(this.email);
-		return matcher.matches();
+	/**
+	 * @param dayUsingTime the dayUsingTime to set
+	 */
+	public void setDayUsingTime(int date, int dayTime) {
+		this.dayUsingTime[0]=date;
+		this.dayUsingTime[1]=dayTime;
 	}
 
+	/**
+	 * @return the studentID
+	 */
 	public String getStudentID() {
 		return studentID;
 	}
 
+	/**
+	 * @param studentID the studentID to set
+	 */
 	public void setStudentID(String studentID) {
 		this.studentID = studentID;
 	}
 
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * @param email the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the status
+	 */
 	public int getStatus() {
 		return status;
 	}
 
+	/**
+	 * @param status the status to set
+	 */
 	public void setStatus(int status) {
 		this.status = status;
 	}
-
-	@Override
-	public String toString() {
-		return "User [studentID=" + studentID + ", email=" + email + ", name=" + name + "]";
+	
+	/**
+	 * @return the cld
+	 */
+	public Calendar getCld() {
+		return cld;
 	}
 
-	public float getFine() {
+	/**
+	 * @param cld the cld to set
+	 */
+	public void setCld(Calendar cld) {
+		this.cld = cld;
+	}
+
+	/**
+	 * @return the fine
+	 */
+	public boolean isFine() {
 		return fine;
 	}
 
-	public void setFine(float fine) {
+	/**
+	 * @param fine the fine to set
+	 */
+	public void setFine(boolean fine) {
 		this.fine = fine;
 	}
+	
+	@Override
+	public String toString() {
+		return "User [studentID=" + studentID + ", email=" + email + ", name=" + name + ", status=" + status + "]";
+	}
 
+	
 }
