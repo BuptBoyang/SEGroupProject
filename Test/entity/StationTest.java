18f3e7874fe19ec1550950defc12bd5cd7b2ee12
BuptBoyang <zby.che9@gmail.com>
2019-05-30 01:23:09 +0800

    Test

--- Test/entity/StationTest.java
+++ Test/entity/StationTest.java
@@ -7,9 +7,35 @@ import org.junit.jupiter.api.Test;
 class StationTest {
 
 	@Test
-	void test() {
+	void testCreateStation() {
 		Station station = new Station();
 		assertEquals(station.getSlot(0).isHasScooter(),true);
+		assertEquals(station.getSlot(0).isLocked(),true);
+		assertEquals(station.getSlot(1).isHasScooter(),true);
+		assertEquals(station.getSlot(2).isHasScooter(),true);
+		assertEquals(station.getSlot(3).isHasScooter(),true);
+		assertEquals(station.getSlot(4).isHasScooter(),false);
+		assertEquals(station.getSlot(5).isHasScooter(),false);
+		assertEquals(station.getSlot(6).isHasScooter(),false);
+		assertEquals(station.getSlot(7).isHasScooter(),false);
+	}
+	
+	@Test
+	void testSlot() {
+		Station station = new Station();
+		station.openSlot(0);
+		assertEquals(station.getSlot(0).isLocked(),false);
+		station.closeSlot(0);
+		assertEquals(station.getSlot(0).isLocked(),true);
+	}
+	
+	@Test
+	void testReset() {
+		Station station = new Station();
+		station.reset(0, true);
+		assertEquals(station.getSlot(0).isHasScooter(),true);
+		station.reset(0, false);
+		assertEquals(station.getSlot(0).isHasScooter(),false);
 	}
 
 }
