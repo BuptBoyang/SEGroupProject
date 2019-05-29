df9c205628694306c0319c88397167c1bd104cbb
BuptBoyang <zby.che9@gmail.com>
2019-05-29 20:22:04 +0800

    Update StationTest.java
    TDD

--- Test/entity/StationTest.java
+++ Test/entity/StationTest.java
@@ -7,38 +7,9 @@ import org.junit.jupiter.api.Test;
 class StationTest {
 
 	@Test
-	void findFreeSlotTest() {
-		Station stationTest = new Station();
-		int empty = stationTest.findFreeSlot(false);
-		int notEmpty = stationTest.findFreeSlot(true);
-		assertEquals(4,empty);
-		assertEquals(0,notEmpty);
-	}
-	
-	@Test
-	void checkToBorrowTest() {
-		Station stationTest = new Station();
-		
-		int slotNo = stationTest.findFreeSlot(true);
-		stationTest.releaseSlot(slotNo);
-		stationTest.pressSimulator(slotNo);
-		stationTest.timeout(slotNo);
-		
-		boolean borrowResult = stationTest.checkToBorrow(slotNo);
-		assertEquals(true,borrowResult);
-	}
-	
-	@Test
-	void checkToReturnTest() {
-		Station stationTest = new Station();
-		
-		int slotNo = stationTest.findFreeSlot(false);
-		stationTest.releaseSlot(slotNo);
-		stationTest.pressSimulator(slotNo);
-		stationTest.timeout(slotNo);
-		
-		boolean returnResult = stationTest.checkToReturn(slotNo);
-		assertEquals(true,returnResult);
+	void test() {
+		Station station = new Station();
+		assertEquals(station.getSlot(0).isHasScooter(),true);
 	}
 
 }
